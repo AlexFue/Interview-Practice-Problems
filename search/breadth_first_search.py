5d92f8ed3be4aca35fe70cbc03330c18a8cf74fd
Alex Espinoza-Fuentes <alexespinoza11f@gmail.com>
2020-11-28 16:55:29 -0800

    example of breadth first search

---
+++ search/breadth_first_search.py
@@ -0,0 +1,47 @@
+Here is an example on how Breadth First Search works on a graph of nodes: 
+
+
+
+
+class Node: 
+  def __init__(self, val, neighbors = []):
+    self.val = val
+    self.neighbors = neighbors
+
+def bfs(s):
+  seen = set() 
+  queue = []
+
+  queue.append(s)
+
+  while queue:
+    cur = queue.pop()
+    if cur not in seen:
+      seen.add(cur)
+      doWork(cur.val)
+    for neighbor in cur.neighbors:
+      if neighbor not in seen:
+        queue.append(neighbor)
+
+def doWork(n):
+  print(n)
+
+a = Node('A')
+e = Node('E')
+d = Node('D')
+c = Node('C')
+b = Node('B')
+g = Node('G')
+f = Node('F')
+h = Node('H')
+
+a.neighbors = [b,e,d,c]
+e.neighbors = [f,a,d]
+d.neighbors = [a,e,h,c]
+c.neighbors = [a,d,b]
+b.neighbors = [g,a,c]
+g.neighbors = [b,f]
+f.neighbors = [g,e,h]
+h.neighbors = [f,d]
+
+bfs(a)
